c3ee3ac155c33a84d226722dbf7ae1dc373f2de4
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-07 20:17:02 +0900

    API作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Choose } from './components/Choose'
-import { Signin } from './components/Signin'
+import { Choose } from './components/Choose';
+import { Signin } from './components/Signin';
+import { useState } from 'react';
 
 
 function App() {
+  const [selectFav, setSelectFav] = useState({
+    bitterness: 3,
+    acidity: 3,
+    sweets: 3,
+    richbody: 3,
+    flavor: 3
+  });
+
+  const scoreBeans = (select) => {
+    let score = 0;
+
+
+  }
+
+
 
   return (
     <>
---
+++ src/server.js
@@ -0,0 +1,147 @@
+const knex = require("./knex.js");
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+app.use("/", express.static("../public"));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    next();
+});
+
+app.use(express.json());
+
+
+// コーヒー豆を全て返す
+app.get("/allbeans", async (req, res) => {
+    const beans = await knex("beans").select("*");
+    res.send(beans)
+})
+
+// 選択した好みに近いコーヒー豆を３つ返す
+// 想定req
+// {
+//     "bitterness": 2, 
+//     "acidity": 2,
+//     "sweets": 2, 
+//     "richbody": 2, 
+//     "flavor": 2
+// }
+app.get("/recommendbeans", async (req, res) => {
+    const { bitterness, acidity, sweets, richbody, flavor } = req.body;
+    const beansdb = await knex("beans").select("*");
+    // console.log(beansdb)
+    await beansdb.map(async (beans) => {
+        let score = 0;
+        if (Math.abs(bitterness - beans.bitterness) <= 1) score++;
+        if (Math.abs(acidity - beans.acidity) <= 1) score++;
+        if (Math.abs(sweets - beans.sweets) <= 1) score++;
+        if (Math.abs(richbody - beans.richbody) <= 1) score++;
+        if (Math.abs(flavor - beans.flavor) <= 1) score++;
+        await knex("beans").where({ id: beans.id }).update({ score: score })
+    })
+    await knex("beans").select("*").orderBy("score", "desc").then(data => res.send(data.slice(0, 3)))
+})
+
+// コーヒー豆を修正する
+// 想定req
+// [
+//     {
+//       "id": 14,
+//       "name": "ブラジル・ショコラ",
+//       "bitterness": 3,
+//       "acidity": 2,
+//       "sweets": 4,
+//       "richbody": 4,
+//       "flavor": 4,
+//       "about": "ビターチョコのような重厚感のある甘味を楽しめます。酸味の苦手な方へ特にオススメです!",
+//       "yen": "税込 1,620円/200g",
+//       "url": "http://umapanera.com/?pid=133804987"
+//   }
+// ]
+app.put("/updatebeans", async (req, res) => {
+    try {
+        await req.body.map(async (data) => {
+            await knex("beans")
+                .where({ id: data.id })
+                .update({
+                    name: data.name,
+                    bitterness: data.bitterness,
+                    acidity: data.acidity,
+                    sweets: data.sweets,
+                    richbody: data.richbody,
+                    flavor: data.flavor,
+                    about: data.about,
+                    yen: data.yen,
+                    img: data.img,
+                    url: data.url
+                })
+        })
+        res.send("コーヒー豆修正完了")
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    };
+})
+
+// コーヒー豆を追加する
+//     {
+//       "name": "ブラジル・ショコラ",
+//       "bitterness": 3,
+//       "acidity": 2,
+//       "sweets": 4,
+//       "richbody": 4,
+//       "flavor": 4,
+//       "about": "ビターチョコのような重厚感のある甘味を楽しめます。酸味の苦手な方へ特にオススメです!",
+//       "yen": "税込 1,620円/200g",
+//       "url": "http://umapanera.com/?pid=133804987"
+//   }
+app.post("/addbeans", async (req, res) => {
+    const data = req.body;
+    try {
+        let id;
+        await knex("beans")
+            .max("id")
+            .then((maxid) => {
+                maxid === null ? (id = 0) : (id = maxid[0].max);
+            });
+        await knex("beans").insert({
+            id: id + 1,
+            name: data.name,
+            bitterness: data.bitterness,
+            acidity: data.acidity,
+            sweets: data.sweets,
+            richbody: data.richbody,
+            flavor: data.flavor,
+            about: data.about,
+            yen: data.yen,
+            img: "beans.jpg",
+            url: data.url,
+            score: 0
+        })
+        res.send("コーヒー豆追加完了");
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    };
+})
+
+//　コーヒー豆を削除する 
+app.delete("/delbeans/:id", async (req, res) => {
+    const delId = req.params.id;
+    try {
+        await knex("beans").where({ id: delId }).del();
+        res.send("削除完了")
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running http://localhost:${PORT}`);
+});
