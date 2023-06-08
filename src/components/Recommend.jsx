ba5cbdf2b3d4d5bf9201174c123fae9df22b0e00
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-08 11:24:00 +0900

    MVP#1

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -1,4 +1,5 @@
 // import App from "../App"
+import { Fragment } from "react"
 
 export const Recommend = (props) => {
 
@@ -9,32 +10,35 @@ export const Recommend = (props) => {
             <h2>おすすめのお豆はこちら</h2>
 
             <table className="ranking">
-                {recommendBeans.map((beans, i) => {
-                    return (
-                        <>
-                            <tr key={i}>
-                                <td>
-                                    <img src={`${process.env.PUBLIC_URL}/img/No${i + 1}.png`} alt="No1"></img>
-                                </td>
-                                <td class="url">
-                                    <a className="url" rel="noreferrer" target="_blank" href={beans.url}>
-                                        + 詳細をみる（外部サイトへ）
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img className="img" src={`${process.env.PUBLIC_URL}/img/${beans.img}`} alt="beansphoto"></img>
-                                </td>
-                                <td>
-                                    <p className="name">{beans.name}</p>
-                                    <p className="about">{beans.about}</p>
-                                    <p className="yen">{beans.yen}</p>
-                                </td>
-                            </tr>
-                        </>
-                    )
-                })}
+                <tbody>
+
+                    {recommendBeans.map((beans, i) => {
+                        return (
+                            <Fragment key={beans.id}>
+                                <tr>
+                                    <td>
+                                        <img src={`${process.env.PUBLIC_URL}/img/No${i + 1}.png`} alt="No1"></img>
+                                    </td>
+                                    <td>
+                                        <a className="url" rel="noreferrer" target="_blank" href={beans.url}>
+                                            + 詳細をみる（外部サイトへ）
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img className="img" src={`${process.env.PUBLIC_URL}/img/${beans.img}`} alt="beansphoto"></img>
+                                    </td>
+                                    <td>
+                                        <p className="name">{beans.name}</p>
+                                        <p className="about">{beans.about}</p>
+                                        <p className="yen">{beans.yen}</p>
+                                    </td>
+                                </tr>
+                            </Fragment>
+                        )
+                    })}
+                </tbody>
             </table>
 
 
