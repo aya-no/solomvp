8b02671ed2840a8d9a24a1823208a8334f19b01a
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-08 15:25:55 +0900

    デプロイ

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use("/", express.static("../public"));
 
 app.use((req, res, next) => {
     // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Origin", "https://solomvp.onrender.com/");
+    res.header("Access-Control-Allow-Origin", "https://solomvp.onrender.com");
     res.header("Access-Control-Allow-Headers", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     next();
